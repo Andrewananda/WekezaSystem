26289b190973b3f58f385244c8b04c9df15b4394
Andrew Ananda <andrewtiema@gmail.com>
2019-09-11 21:21:49 +0300

    feature/login user

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,10 +4,11 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{ asset('assets/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+                <img src="" class="img-circle" alt="User Image">
+
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+{{--                <p>{{\Illuminate\Support\Facades\Auth::user()->name}}</p>--}}
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 
-Route::get('/', function () {
-    return view('add-member');
-});
+
+Route::get('/', 'HomeController@index')->name('home');
+
 
 Route::get('/add-member', [
     'uses'=>'UserController@index',
