dc8e2c87a5b133a4abc7142d8155d5924f208ff3
Andrew Ananda <andrewtiema@gmail.com>
2019-09-21 13:43:25 +0300

    fix/sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -18,7 +18,7 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
+            <li >
                 <a href="{{ route('home') }}">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                 </a>
@@ -38,6 +38,7 @@
                 </ul>
             </li>
             @endif
+            @if(Auth::User()->can('add-project'))
             <li class="treeview">
                 <a href="#">
                     <i class="fa fa-bookmark"></i>
@@ -51,7 +52,8 @@
                     <li><a href="pages/UI/icons.html"><i class="fa fa-bookmark"></i> All Projects</a></li>
                 </ul>
             </li>
-
+            @endif
+            @if(Auth::user()->can('add-permissions'))
             <li class="treeview">
                 <a href="#">
                     <i class="fa fa-bookmark"></i>
@@ -66,6 +68,7 @@
                     <li><a href="{{ route('register.permission') }}"><i class="fa fa-bookmark"></i> Add Permission</a></li>
                 </ul>
             </li>
+            @endif
 
             <li class="treeview">
                 <a href="#">
@@ -74,9 +77,11 @@
               <i class="fa fa-angle-left pull-right"></i>
             </span>
                 </a>
+                @if(Auth::user()->can('add-member'))
                 <ul class="treeview-menu">
                     <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> Remove Member</a></li>
                     <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Assign Penalty To Member</a></li>
+                    @endif
                     <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> All Punished Members</a></li>
                 </ul>
             </li>
@@ -88,7 +93,9 @@
             </span>
                 </a>
                 <ul class="treeview-menu">
+                    @if(Auth::user()->can('add-contributions'))
                     <li><a href="{{ route('contribute') }}"><i class="fa fa-circle-o"></i> Add Contribution</a></li>
+                    @endif
                     <li><a href="{{ route('all.contributions') }}"><i class="fa fa-circle-o"></i> All Contributions</a></li>
                     <li><a href="{{ route('my.contributions') }}"><i class="fa fa-circle-o"></i> My Contributions</a></li>
 
