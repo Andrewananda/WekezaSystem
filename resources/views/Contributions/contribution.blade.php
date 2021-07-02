9ccfca5d4c92a931a4d8b80c1e37fabe21d22635
Andrew Tiema <andrewtiema@gmail.com>
2021-07-02 14:34:21 +0300

    fix/contribution screen

--- resources/views/Contributions/contribution.blade.php
+++ resources/views/Contributions/contribution.blade.php
@@ -1,51 +1,52 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="box box-primary">
-        <div class="box-header with-border">
-            <h3 class="box-title">Add Member Contribution</h3>
-        </div>
-        <!-- /.box-header -->
-        <!-- form start -->
-        <form method="post" action="{{ route('member.contribution') }}">
-            @csrf
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="first_name">Name</label>
-                            <select name="user_id" id="user_id">
-                                <option value="">Member name</option>
-                                @foreach($users as $user_name)
-                                <option name="user_id" value="{{ $user_name->id }}">{{ $user_name->name }}</option>
-                                    @endforeach
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="date">Date</label>
-                                <input type="date"  name="date" id="date">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="amount">Amount</label>
-                            <input type="text" name="amount"  id="amount" placeholder="Enter Amount">
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="box-body">
-                <button type="submit" style="margin-left:50%;margin-right:50%;display:block;margin-top:0%;margin-bottom:0%" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h3 class="box-title">Add Member Contribution</h3>
     </div>
     @include('exception.error')
+    <form enctype="multipart/form-data" method="post" action="{{ route('member.contribution') }}">
+        @csrf
+        <div class="row">
+
+            <div class="col-md-6">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label>User</label>
+                        <select class="form-control select2" name="user_id" id="user_id" style="width: 100%;">
+                            <option selected="selected">Member Name</option>
+                            @foreach($users as $user)
+                                <option value="{{ $user->id }}">{{$user->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="date">Date</label>
+                        <input type="date" name="date" class="form-control" id="date" placeholder="Date">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="amount">Amount</label>
+                        <input type="text" name="amount" class="form-control"  id="amount" placeholder="Enter Amount">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="box-body">
+            <button type="submit" style="display:block;margin: 0% 35%; width: 300px" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</div>
 
 @endsection
